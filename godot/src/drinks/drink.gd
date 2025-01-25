617e3f9f84a8a4cde07c4949c7c3c86a8d87d305
Ángela <angela.lbsr@gmail.com>
2025-01-25 21:10:10 +0100

    force ready sequences from debug panel

--- godot/src/drinks/drink.gd
+++ godot/src/drinks/drink.gd
@@ -2,5 +2,5 @@ class_name Drink extends RefCounted
 
 var ingridients: Dictionary # [Types.NodeType, int] Quantity of each ingridient
 
-func _to_string():
-	return ingridients
+func _to_string() -> String:
+	return str(ingridients)
--- godot/src/narrative/debug/sequence_state.gd
+++ godot/src/narrative/debug/sequence_state.gd
@@ -14,8 +14,49 @@ func _ready() -> void:
 		
 	for r in sequence.requirements:
 		var requirement = CheckBox.new()
-		requirement.disabled = true
 		requirement.text = r.get_name()
 		requirement.button_pressed = r.is_met
+		requirement.toggled.connect(_on_requirement_toggled.bind(r))
+		r.status_changed.connect(_on_requirement_status_changed.bind(requirement))
 		%Requirements.add_child(requirement)
+	
+	sequence.ready_changed.connect(func(x): %ReadyButton.button_pressed = x)
+	
+
+func _force_meet_requirement(requirement: SequenceRequirement) -> void:
+	if not requirement.is_met:
+		if requirement is CharacterPresentSequenceRequirement:
+			if requirement.should_be_in_scene:
+				Globals.cafe.seat_character(Narrative.character_by(requirement.character_id))
+			else:
+				Logger.warn("removing character from scene not implemented")
+		if requirement is TitlePassedSequenceRequirement:
+			Narrative.title_passed.emit(requirement.sequence_id)
+		if requirement is FlagSetSequenceRequirement:
+			Narrative.set_flag(requirement.flag, requirement.should_be_set)
+	
+
+func _on_requirement_status_changed(status: bool, node: CheckBox) -> void:
+	node.button_pressed = status
+	
+
+func _on_requirement_toggled(toggled_on: bool, requirement: SequenceRequirement) -> void:
+	if toggled_on:
+		_force_meet_requirement(requirement)
+	
+
+func _on_finished_button_toggled(toggled_on):
+	if toggled_on:
+		Narrative.current_sequence = sequence
+		Narrative.title_passed.emit(sequence.id)
+		sequence.finish()
+	
+
+func _on_ready_button_toggled(toggled_on):
+	if toggled_on:
+		for requirement in sequence.requirements:
+			_force_meet_requirement(requirement)
 		
+	else:
+		sequence.is_ready = sequence._check_ready()
+		%ReadyButton.button_pressed = sequence.is_ready
