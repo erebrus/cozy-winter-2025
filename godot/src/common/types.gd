aa1b602d31f31ab8525457fc299446daf81a61ed
Ángela <angela.lbsr@gmail.com>
2025-01-25 13:29:48 +0100

    surfaces

--- godot/src/common/types.gd
+++ godot/src/common/types.gd
@@ -13,9 +13,22 @@ enum Item {
 	FIREPLACE
 }
 
+enum SurfaceId {
+	COUNTER,
+	WALL,
+	DOOR,
+	FIREPLACE
+}
+
+
 func npc_key(value: NPC) -> String:
 	return NPC.keys()[value]
 	
 
 func item_key(value: Item) -> String:
 	return Item.keys()[value]
+	
+
+func surface_key(value: SurfaceId) -> String:
+	return SurfaceId.keys()[value]
+	
---
+++ godot/src/environment/surface.gd
@@ -0,0 +1,4 @@
+class_name Surface extends Area2D
+
+
+@export var id: Types.SurfaceId
