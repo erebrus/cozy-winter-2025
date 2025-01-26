ebbc4b418ef0b56ad9c43c25631ce14cf5cbcfec
Ángela <angela.lbsr@gmail.com>
2025-01-26 12:33:09 +0100

    integrate bg (temp?)

--- godot/src/camera_2d.gd
+++ godot/src/camera_2d.gd
@@ -1,5 +1,10 @@
 extends Camera2D
 
+func _ready():
+	await get_tree().create_timer(0.1).timeout
+	position_smoothing_enabled = true
+	
+
 func _process(_delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	global_position.x = mouse_position.x
